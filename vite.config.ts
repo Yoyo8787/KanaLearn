9da3e609bc006075e1d5350f3ac8796865d44a65
r12725065 <r12725065@ntu.edu.tw>
2025-12-24 21:32:15 +0800

    feat: update vite.config and package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // GitHub Pages 部署於 yoyo8787.github.io/KanaLearn，需要指定 base 才能正確載入資源
+  base: "/KanaLearn/",
   plugins: [react(), tailwindcss()],
 });
